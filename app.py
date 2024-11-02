01433aad124de44cc43479332e6859be0a371a90
Moisikk <ionutmoise77@gmail.com>
2024-11-02 11:40:30 +0200

    al 3 lea commit
    Signed-off-by: Moisikk <ionutmoise77@gmail.com>

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Dragnea este un prieten sa zicem bun"
+    return "Python docker localhosted at :8000"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
